d9917c9300e7b701f2c85e6b9953c9f5efec8468
emp1tsu <oneok0813@gmail.com>
2021-04-01 23:09:13 +0900

    list note

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import { BsPencilSquare } from "react-icons/bs";
+import { LinkContainer } from "react-router-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
@@ -34,7 +36,29 @@ export default function Home() {
   }
 
   function renderNotesList(notes) {
-    return null;
+    return (
+      <>
+        <LinkContainer to="/notes/new">
+          <ListGroup.Item action className="py-3 text-nowrap text-truncate">
+            <BsPencilSquare size={17} />
+            <span className="ml-2 font-weight-bold">Create a new note</span>
+          </ListGroup.Item>
+        </LinkContainer>
+        {notes.map(({ noteId, content, createdAt }) => (
+          <LinkContainer key={noteId} to={`/notes/${noteId}`}>
+            <ListGroup.Item action>
+              <span className="font-weight-bold">
+                {content.trim().split("\n")[0]}
+              </span>
+              <br />
+              <span className="text-muted">
+                Created: {new Date(createdAt).toLocaleString()}
+              </span>
+            </ListGroup.Item>
+          </LinkContainer>
+        ))}
+      </>
+    );
   }
 
   function renderLander() {
